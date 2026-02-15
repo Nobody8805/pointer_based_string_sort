9ac54e69078edb8dadc9f321f58a2e12ff116679
Nobody8805 <ac88.chauhan@gmail.com>
2026-02-15 22:39:19 +0530

    Initial commit

---
+++ src/readlines.c
@@ -0,0 +1,15 @@
+/*
+* FOCUS POINTS FOR THIS MODULE:-
+* 1. User types in the strings. Each string is stored in a common char arr sequentially.
+* 2. Each new line '\n' or EOF is replaced by a null terminator '\0' to keep track of a new str.
+* 3. We store the ptr to the start of each str in a ptr array.
+*
+*
+* 2 Functions should be there, one that r b
+*/
+
+#include <stdio.h>
+
+#define CHARBUFFER 10000
+
+
